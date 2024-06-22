054666fa4879963091beba50daed49911bc743b3
Ama-Annor <136464888+Ama-Annor@users.noreply.github.com>
2024-06-22 09:57:57 +0000

    Add files via upload

---
+++ website.py
@@ -0,0 +1,29 @@
+import steamlit as st
+import joblib
+import pandas as pd
+
+model = joblib.load('best_model.pkl')
+scalar = joblib.load('scaler.pkl')
+
+st.title('FIFA Prediction')
+
+st.write('This is a simple FIFA prediction model. Please enter the required details to get the prediction')
+
+#Figure a way to get input from the user
+
+def training_attributes():
+    at1 = st.sidebar
+    at2 = st.sidebar
+
+    values = {'name of columns I am using': at1, 
+              
+
+    }
+
+    characteristics = pd.DataFrame(values, index = [0])
+    return characteristics
+
+#Use the scaler and model to Display the output to the user
+
+
+
