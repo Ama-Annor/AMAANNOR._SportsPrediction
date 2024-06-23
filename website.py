d7a0c7bdafc2e9922117416c34dcff6687ecd2f9
Ama-Annor <136464888+Ama-Annor@users.noreply.github.com>
2024-06-23 13:54:32 +0000

    Update website.py

--- website.py
+++ website.py
@@ -21,16 +21,43 @@ st.title('FIFA Player Rating Prediction')
 st.write('Enter player attributes to predict their FIFA rating.')
 
 # Manually specify features based on your Jupyter notebook
-features = ['overall', 'height_cm', 'weight_kg', 'age', 'physic', 'power_strength',
-            'power_jumping', 'movement_agility', 'movement_balance', 'dribbling',
-            'skill_dribbling', 'skill_ball_control', 'shooting', 'passing',
-            'skill_long_passing', 'skill_fk_accuracy', 'attacking_crossing',
-            'attacking_finishing', 'attacking_heading_accuracy', 'attacking_short_passing',
-            'attacking_volleys', 'mentality_aggression', 'mentality_interceptions',
-            'mentality_positioning', 'mentality_vision', 'mentality_penalties',
-            'mentality_composure', 'movement_reactions', 'pace', 'movement_acceleration',
-            'movement_sprint_speed', 'power_stamina', 'power_shot_power', 'power_long_shots',
-            'defending']
+features = [
+    'age', 
+    'height_cm', 
+    'weight_kg', 
+    'pace', 
+    'shooting', 
+    'passing', 
+    'dribbling', 
+    'defending', 
+    'physic', 
+    'attacking_crossing', 
+    'attacking_finishing', 
+    'attacking_heading_accuracy', 
+    'attacking_short_passing', 
+    'attacking_volleys', 
+    'skill_dribbling', 
+    'skill_fk_accuracy', 
+    'skill_long_passing', 
+    'skill_ball_control', 
+    'movement_acceleration', 
+    'movement_sprint_speed', 
+    'movement_agility', 
+    'movement_reactions', 
+    'movement_balance', 
+    'power_shot_power', 
+    'power_jumping', 
+    'power_stamina', 
+    'power_strength', 
+    'power_long_shots', 
+    'mentality_aggression', 
+    'mentality_interceptions', 
+    'mentality_positioning', 
+    'mentality_vision', 
+    'mentality_penalties', 
+    'mentality_composure'
+]
+
 
 # Function to get user inputs
 def get_user_input(features):
